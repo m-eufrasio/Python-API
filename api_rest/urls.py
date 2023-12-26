00f339425e57c8aac064107d6e684a5e0c3f3296
Matheus Eufrasio <math.eufrasio@hotmail.com>
2023-12-26 16:36:10 -0300

    feat: added simple jwt

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
   path('', views.get_users, name='get_all_users'),
   path('user/<str:nick>', views.get_by_nick),
-  path('data/', views.user_manager) # A barra no final do data não é obrigatório
+  path('data/', views.user_manager), # A barra no final do data não é obrigatório
+  
+  path('token/', MyTokenObtainPairView.as_view(), name='my_token_obtain_pair_view'),
+  path('token/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
 ]
